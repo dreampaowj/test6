d5202367ab184c05e9a444913d9c58b2897bea50
dreampaowj <dreampaowj@hotmail.com>
2021-08-20 11:42:51 +0900

    在master分支上添加了A2

---
+++ src/main/java/com/example/test6/bean/A2.java
@@ -0,0 +1,18 @@
+package com.example.tst6.bean;
+
+public class A2 {
+    private String name;
+
+    private int age;
+
+    private String city;
+
+    public A2() {
+    }
+
+    public A2(String name, int age, String city) {
+        this.name = name;
+	this.age = age;
+	this.city = city;
+    }
+}
